f0330cc3483bf3392d4028e55f1e4476c40ae352
Moh88CS <mohamed.abdulal88@gmail.com>
2023-05-03 23:22:08 -0400

    Test validation

--- public/scripts.js
+++ public/scripts.js
@@ -5,11 +5,15 @@
 
 function showHideShots() {
     let check = document.getElementById('opponent');
+    let rpsGame = document.getElementById('rps').checked;
 
-    if(check.checked == true) {
-        $('.shots').show()
+    if (check.checked == true && rpsGame == true) {
+        $('.shots').show();
+        $('.rpsls').hide(); // Hide Lizard and Spock when playing RPS against an opponent
+    } else if (check.checked == true && rpsGame == false) {
+        $('.shots').show();
     } else {
-        $('.shots').hide()
+        $('.shots').hide();
     }
 }
 
@@ -17,18 +21,49 @@ function startOver() {
     document.getElementById('userinput').reset();
     showHideShots();
     
-}
+    // Clear the results
+    document.getElementById('results').innerHTML = '';
+  }
+
+function resetShotSelection() {
+    let game = $('input[type=radio][name=game]:checked').val();
+    let firstShot;
+  
+    if (game === 'rps') {
+      firstShot = 'rock';
+    } else {
+      firstShot = 'lizard';
+    }
+  
+    // Reset the selected shot to the first valid option for the chosen game
+    $('input[type=radio][name=shot]').prop('checked', false);
+    $(`input[type=radio][name=shot][value=${firstShot}]`).prop('checked', true);
+  }
+  
+  
 
 async function playGame() {
     let game = $('input[type=radio][name=game]:checked').val();
     let shot = $('input[type=radio][name=shot]:checked').val();
-
-    let baseurl = window.location.href + 'app/'
-    console.log(baseurl)
-    let url = baseurl + game + '/play/' + shot
-    console.log(url)
-
-    let response = await fetch(url)
-    let result = await response.json()
-    console.log(result)
-}
+  
+    let baseurl = window.location.href + 'app/';
+    console.log(baseurl);
+    let url = baseurl + game + '/play/' + shot;
+    console.log(url);
+  
+    let response = await fetch(url);
+    let result = await response.json();
+    console.log(result);
+  
+    // Display the results on the page
+    let resultsDiv = document.getElementById('results');
+    if (result.error) {
+      resultsDiv.innerHTML = result.error;
+    } else {
+      let you = `You: ${result.player}`;
+      let opponent = `Opponent: ${result.opponent}`;
+      let outcome = `Result: You ${result.result.toUpperCase()}!`;
+      resultsDiv.innerHTML = `${you}<br>${opponent}<br>${outcome}`;
+    }
+  }
+  
